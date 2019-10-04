0811bcee3105a7d498ed41d9cbe58d6ce6ce0b88
Mihail Hamhoev <hamhoevmb@gmail.com>
2019-10-04 11:31:00 +0700

    property group changed with RootNamespace skipping

--- Commands/DeletePropertyGroupsCommand.cs
+++ Commands/DeletePropertyGroupsCommand.cs
@@ -23,12 +23,19 @@ namespace Targets.Commands
 
                     foreach (var property in project.Xml.PropertyGroups)
                     {
-                        if (property.Children.Count(p =>
-                                (string) p.AsDynamic().Name == "OutputType") > 0)
+                        if (property.Children.Count(p => (string) p.AsDynamic().Name == "OutputType") > 0 ||
+                            property.Children.Count(p => (string)p.AsDynamic().Name == "RootNamespace") > 0)
                         {
                             property.Children
-                                .Where(p => (string) p.AsDynamic().Name != "OutputType")
-                                .ForEach(x => property.RemoveChild(x));
+                                .Where(p => (string) p.AsDynamic().Name != "OutputType" || 
+                                            (string)p.AsDynamic().Name != "RootNamespace" || 
+                                            (string)p.AsDynamic().Name != "ProjectGuid" || 
+                                            (string)p.AsDynamic().Name != "NuGetPackageImportStamp")
+                                .ForEach(x =>
+                                {
+                                    property.RemoveChild(x);
+                                    Logger.Info($"Removed property group child");
+                                });
                         }
                         else
                         {
